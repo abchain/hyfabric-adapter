b84615f8667cad209b883b56275c4c54b3d2df1a
yinminghao <yinminghao@stringon.com>
2020-06-09 11:35:24 +0800

    refactor: remove useless files.

--- client/peerex/peerclient.go
+++
@@ -1,9 +0,0 @@
-package peerex
-
-import (
-	pb "github.com/hyperledger/fabric/protos/peer"
-)
-
-func (peer *PeerEnv) NewEndorserClient() pb.EndorserClient {
-	return pb.NewEndorserClient(peer.Connect)
-}
