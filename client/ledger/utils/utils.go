af53c0e4213689ea42ec84e698c4daf2309508b7
yinminghao <yinminghao@stringon.com>
2020-07-09 15:37:10 +0800

    fix: use errors.Warpf install of fmt.Errorf

--- client/ledger/utils/utils.go
+++ client/ledger/utils/utils.go
@@ -6,25 +6,22 @@ import (
 	"github.com/golang/protobuf/proto"
 	"github.com/hyperledger/fabric-protos-go/common"
 	"github.com/hyperledger/fabric-protos-go/peer"
+	"github.com/pkg/errors"
 )
 
 // GetEnvelopeFromBlock gets an envelope from a block's Data field.
 func GetEnvelopeFromData(data []byte) (*common.Envelope, error) {
 	// Block always begins with an envelope
-	var err error
 	env := &common.Envelope{}
-	if err = proto.Unmarshal(data, env); err != nil {
-		return nil, fmt.Errorf("error unmarshaling envelope: %v", err)
-	}
-
-	return env, nil
+	err := proto.Unmarshal(data, env)
+	return nil, errors.Wrapf(err, "error unmarshaling envelope")
 }
 
 // GetProposal returns a Proposal message from its bytes
 func GetProposal(propBytes []byte) (*peer.Proposal, error) {
 	prop := &peer.Proposal{}
 	err := proto.Unmarshal(propBytes, prop)
-	return prop, fmt.Errorf("error unmarshaling proposal: %v", err)
+	return prop, errors.Wrapf(err, "error unmarshaling proposal")
 }
 
 // GetChaincodeInvocationSpec get the ChaincodeInvocationSpec from the proposal
@@ -42,42 +39,42 @@ func GetChaincodeInvocationSpec(prop *peer.Proposal) (*peer.ChaincodeInvocationS
 	}
 	cis := &peer.ChaincodeInvocationSpec{}
 	err = proto.Unmarshal(ccPropPayload.Input, cis)
-	return cis, fmt.Errorf("error unmarshaling chaincodeInvocationSpec: %v", err)
+	return cis, errors.Wrapf(err, "error unmarshaling chaincodeInvocationSpec")
 }
 
 // GetChaincodeAction gets the ChaincodeAction given chaicnode action bytes
 func GetChaincodeAction(caBytes []byte) (*peer.ChaincodeAction, error) {
 	chaincodeAction := &peer.ChaincodeAction{}
 	err := proto.Unmarshal(caBytes, chaincodeAction)
-	return chaincodeAction, fmt.Errorf("error unmarshaling ChaincodeAction: %v", err)
+	return chaincodeAction, errors.Wrapf(err, "error unmarshaling ChaincodeAction")
 }
 
 // GetChaincodeEvents gets the ChaincodeEvents given chaincode event bytes
 func GetChaincodeEvents(eBytes []byte) (*peer.ChaincodeEvent, error) {
 	chaincodeEvent := &peer.ChaincodeEvent{}
 	err := proto.Unmarshal(eBytes, chaincodeEvent)
-	return chaincodeEvent, fmt.Errorf("error unmarshaling ChaicnodeEvent: %v", err)
+	return chaincodeEvent, errors.Wrapf(err, "error unmarshaling ChaicnodeEvent")
 }
 
 // GetPayload Get Payload from Envelope message
 func GetPayload(e *common.Envelope) (*common.Payload, error) {
 	payload := &common.Payload{}
 	err := proto.Unmarshal(e.Payload, payload)
-	return payload, fmt.Errorf("error unmarshaling Payload: %v", err)
+	return payload, errors.Wrapf(err, "error unmarshaling Payload")
 }
 
 // UnmarshalChannelHeader returns a ChannelHeader from bytes
 func UnmarshalChannelHeader(bytes []byte) (*common.ChannelHeader, error) {
 	chdr := &common.ChannelHeader{}
 	err := proto.Unmarshal(bytes, chdr)
-	return chdr, fmt.Errorf("error unmarshaling ChannelHeader:  %v", err)
+	return chdr, errors.Wrapf(err, "error unmarshaling ChannelHeader")
 }
 
 // GetTransaction Get Transaction from bytes
 func GetTransaction(txBytes []byte) (*peer.Transaction, error) {
 	tx := &peer.Transaction{}
 	err := proto.Unmarshal(txBytes, tx)
-	return tx, fmt.Errorf("error unmarshaling Transaction: %v", err)
+	return tx, errors.Wrapf(err, "error unmarshaling Transaction")
 
 }
 
@@ -85,26 +82,26 @@ func GetTransaction(txBytes []byte) (*peer.Transaction, error) {
 func GetChaincodeActionPayload(capBytes []byte) (*peer.ChaincodeActionPayload, error) {
 	cap := &peer.ChaincodeActionPayload{}
 	err := proto.Unmarshal(capBytes, cap)
-	return cap, fmt.Errorf("error unmarshaling ChaincodeActionPayload: %v", err)
+	return cap, errors.Wrapf(err, "error unmarshaling ChaincodeActionPayload")
 }
 
 // GetProposalResponsePayload gets the proposal response payload
 func GetProposalResponsePayload(prpBytes []byte) (*peer.ProposalResponsePayload, error) {
 	prp := &peer.ProposalResponsePayload{}
 	err := proto.Unmarshal(prpBytes, prp)
-	return prp, fmt.Errorf("error unmarshaling ProposalResponsePayload: %v", err)
+	return prp, errors.Wrapf(err, "error unmarshaling ProposalResponsePayload")
 }
 
 // GetHeader Get Header from bytes
 func GetHeader(bytes []byte) (*common.Header, error) {
 	hdr := &common.Header{}
 	err := proto.Unmarshal(bytes, hdr)
-	return hdr, fmt.Errorf("error unmarshaling Header: %v", err)
+	return hdr, errors.Wrapf(err, "error unmarshaling Header")
 }
 
 // GetChaincodeProposalPayload Get ChaincodeProposalPayload from bytes
 func GetChaincodeProposalPayload(bytes []byte) (*peer.ChaincodeProposalPayload, error) {
 	cpp := &peer.ChaincodeProposalPayload{}
 	err := proto.Unmarshal(bytes, cpp)
-	return cpp, fmt.Errorf("error unmarshaling ChaincodeProposalPayload: %v", err)
+	return cpp, errors.Wrapf(err, "error unmarshaling ChaincodeProposalPayload")
 }
